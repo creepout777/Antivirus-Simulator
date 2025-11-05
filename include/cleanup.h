886d873764239e613fb14aba9ad4d7ff7717da61
Creepout <122195548+creepout777@users.noreply.github.com>
2025-11-05 17:23:21 +0100

    Enhance comments for purge_all function
    Added comments to clarify the purpose of purge_all function.

--- include/cleanup.h
+++ include/cleanup.h
@@ -1,7 +1,10 @@
 #ifndef CLEANUP_H
 #define CLEANUP_H
 
-
+/* purge_all: - Appeler free_list pour Clean, Suspect, Quarantine 
+              - Appeler free_sigs pour Signatures 
+              - Mettre tous les pointeurs à NULL 
+*/
 void purge_all(void);
 
 #endif
